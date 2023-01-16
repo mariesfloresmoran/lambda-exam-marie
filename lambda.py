e9b3436af67a6f9d7b53be48f5dfe1330fb1760b
MarieSFloresM <78627002+mariesfloresmoran@users.noreply.github.com>
2023-01-16 18:38:10 -0500

    Lambda

---
+++ lambda.py
@@ -0,0 +1,20 @@
+import json 
+import os 
+import mercadopago  
+ 
+ 
+def lambda_handler(event, context): 
+    sdk= mercadopago.SDK(os.environ["ACCESS_TOKEN"]) 
+    bodyGet = event 
+    
+    payment_response = sdk.payment().create(bodyGet["paymentData"]) 
+    payment = payment_response["response"] 
+     
+    preference_response = sdk.preference().create(bodyGet["productData"]) 
+    preference = preference_response["response"] 
+ 
+ 
+    return { 
+        'statusCode': 200, 
+        'body': json.dumps(preference) 
+    }
